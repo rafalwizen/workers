6caccb4fcafa5f79a1a0b87986d787bb0f645873
Rafal Wizen <rafal.wizen@gmail.com>
2019-11-29 20:59:59 +0100

    added first security accesses

---
+++ src/main/java/wizen/rafal/workers/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package wizen.rafal.workers.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
