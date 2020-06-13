357dd69a573cc1c0d6548a5c96b3de950267f803
Rafal Wizen <rafal.wizen@gmail.com>
2020-06-13 20:25:26 +0200

    add custom error controller

---
+++ src/main/java/wizen/rafal/workers/controller/CustomErrorController.java
@@ -0,0 +1,20 @@
+package wizen.rafal.workers.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController{
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+
+
+	@RequestMapping("/error")
+	public String handleError() {
+		return "error";
+	}
+}
